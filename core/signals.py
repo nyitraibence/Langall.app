3f31acf3537a32a7d67982fdbd75016803b6407c
Nyitrai Bence <nyitrai.done@gmail.com>
2019-10-26 12:33:19 +0200

    Regular signup's greeting mail func added

--- core/signals.py
+++ core/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import Signal
 from django.dispatch import receiver
 from .models import CustomUser
+# for reciever functions:
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 
 # NOTE: All created signals have to be imported in apps.py !
@@ -21,4 +24,15 @@ def welcome_new_user(sender, **kwargs):
     print('New user saved!')
     print(new_user.last_name, new_user.first_name)
     print(new_user.email)
+    subject = '🎉 Köszönjük, hogy regisztráltál!'
+    message = render_to_string('email/welcome_new_user.html', {
+        'new_user': new_user,
+    })
+    to_email = new_user.email
+    email = EmailMessage(
+                subject, message, to=[to_email]
+    )
+    email.content_subtype = "html"          # important addition for html email to render !
+    email.send()
+    print('greeting email: sent >>>')
     print('================')
