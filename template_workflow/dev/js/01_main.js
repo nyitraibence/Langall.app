1b1fc26a415cee4ff03b7b724864f2c3cdea74da
Nyitrai Bence <nyitrai.done@gmail.com>
2019-11-02 16:13:48 +0100

    styling works

--- template_workflow/dev/js/01_main.js
+++ template_workflow/dev/js/01_main.js
@@ -9,3 +9,8 @@ $('.jarallax').jarallax({
     speed: 0.5,
     disableParallax: /iPad|iPhone|iPod|Android/
 });
+
+$('.jarallax-icon').jarallax({
+    speed: .5,
+    disableParallax: /iPad|iPhone|iPod|Android/
+});
