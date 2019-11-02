db24871f0a326c3e4e2f20c67a6058e5db249faf
Nyitrai Bence <nyitrai.done@gmail.com>
2019-11-02 21:30:47 +0100

    implementing charts

---
+++ template_workflow/dev/js/05_charts.js
@@ -0,0 +1,47 @@
+// var ctx = document.getElementById('languages-chart');
+// var opacity_1 = 0.3
+// var myChart = new Chart(ctx, {
+//     type: 'horizontalBar',
+//     data: {
+//         labels: ['Angol', 'Német', 'Spanyol', 'Francia', 'Orosz', 'Olasz'],
+//         datasets: [{
+//             label: '# legnépszerűbb nyelvek',
+//             data: [12, 9, 10, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, ' + opacity_1 + ')',
+//                 'rgba(54, 162, 235, ' + opacity_1 + ')',
+//                 'rgba(255, 206, 86, ' + opacity_1 + ')',
+//                 'rgba(75, 192, 192, ' + opacity_1 + ')',
+//                 'rgba(153, 102, 255, ' + opacity_1 + ')',
+//                 'rgba(255, 159, 64, ' + opacity_1 + ')'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 },
+//                 gridLines: {
+//                     offsetGridLines: true
+//                 }
+//             }]
+//             ,
+//             xAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
