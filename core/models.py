8f9c6e4b51bf2ea4fee189e881a6460bae451e72
Nyitrai Bence <nyitrai.done@gmail.com>
2019-11-03 02:39:08 +0100

    clear unused comments

--- core/models.py
+++ core/models.py
@@ -27,16 +27,3 @@ class CustomUser(AbstractCUser):
     is_premium = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
-
-# class TeacherProfile(models.Model):
-#     user = models.OneToOneField(
-#         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='teacherprofile')
-#     introduction = models.TextField(blank=True, max_length=600)
-#     base_price = models.PositiveSmallIntegerField(blank=True, null=True)
-#     teach_lang_1 = models.CharField(blank=True, max_length=3, choices=LANGUAGE)
-#     teach_lang_2 = models.CharField(blank=True, max_length=3, choices=LANGUAGE)
-#     teach_lang_3 = models.CharField(blank=True, max_length=3, choices=LANGUAGE)
-
-#     def __str__(self):
-#         return 'Information'
-
